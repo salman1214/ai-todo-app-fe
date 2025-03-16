cee5860aef495542c9761a73486f300dd855a050
meesum1214 <103431209+salman1214@users.noreply.github.com>
2025-03-16 22:15:06 +0500

    GoogleGenerativeAI error resolved

--- src/app/api/todo-ai/route.js
+++ src/app/api/todo-ai/route.js
@@ -1,10 +1,4 @@
-import { createTodo, deleteTodoById, getAllTodos, searchTodosByKeyword, updateTodoById } from "@/app/action";
 import { NextResponse } from "next/server";
-
-
-
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export async function POST(request, res) {
     const data = await request.json();
