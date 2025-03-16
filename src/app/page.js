6244ef4e2689680e78bcf7fae9e84e8cfba460e3
meesum1214 <103431209+salman1214@users.noreply.github.com>
2025-03-16 22:18:46 +0500

    middleware commented

--- src/app/page.js
+++ src/app/page.js
@@ -28,12 +28,12 @@ export default function Home() {
 
   return (
     <div className="h-screen">
-      <Button
+      {/* <Button
         className="absolute top-3 left-[47%] h-10 w-20 cursor-pointer active:translate-y-[1px]"
         onClick={onLogout}
       >
         Logout
-      </Button>
+      </Button> */}
       <ResizablePanelGroup direction="horizontal">
         <ResizablePanel>
           <ChatBot refreshTodos={refreshTodos} />
--- src/middleware.js
+++ src/middleware.js
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
     // If the user is already logged in, redirect them to the homepage
-    if (!request.cookies.has('ai-todo-token')) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
+    // if (!request.cookies.has('ai-todo-token')) {
+    //     return NextResponse.redirect(new URL('/login', request.url))
+    // }
 }
 
 // See "Matching Paths" below to learn more
